6b341d290a2fe782556ef653391371b41d066e3a
Dirk Avery <dirk.avery@gmail.com>
2018-06-19 19:39:17 -0400

    Travis/AppVeyor improvements, code cleanup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """pyppyn setup script."""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals, with_statement)
+
 from setuptools import setup
 
 if __name__ == "__main__":
--- tests/minipippy/src/minipippy/__init__.py
+++ tests/minipippy/src/minipippy/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""minipippy test module."""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals, with_statement)
 
 if __name__ == "__main__":
 
